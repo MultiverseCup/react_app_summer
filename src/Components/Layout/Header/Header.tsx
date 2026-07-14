47edcbafd9f4e21fe68812e0fa73eaf9210a3e2f
MultiverseCup <artur.gareev.06.10@gmail.com>
2026-07-14 14:04:03 +0500

    Images styles fixed

--- src/Components/Layout/Header/Header.tsx
+++ src/Components/Layout/Header/Header.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
       <div className={styles.logo}>
         <Link to="/">
           <img
-            src="/images/logo.svg"
+            src="/images/logo.png"
             alt="Логотип"
             className={styles.logoImage}
           />
@@ -36,7 +36,7 @@ export const Header = () => {
       <div className={styles.header__right}>
         <div className={styles.location}>
           <img
-            src="/images/location.svg"
+            src="/images/location.png"
             alt=""
             className={styles.locationIcon}
           />
